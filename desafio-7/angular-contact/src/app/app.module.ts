c1dc74b445f68801d0572ff07cb27db46bebbae2
Eduardo Pollastrini Barbagallo <epb.01@hotmail.com>
2022-08-18 14:39:22 -0300

    modificando

---
+++ desafio-7/angular-contact/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContactFormModule } from './pages/contact-form/contact-form.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MaterialExampleModule } from '../material.module';
+import { MatMenuModule } from '@angular/material/menu';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ContactFormModule,
+    BrowserAnimationsModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    MatMenuModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
